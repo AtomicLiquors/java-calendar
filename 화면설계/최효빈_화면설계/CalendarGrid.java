c65600ed4a9bda4fa8d78eac9057f6356c3c4f1e
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2022-03-21 15:50:37 +0900

    Add files via upload

--- 화면설계/최효빈_화면설계/CalendarGrid.java
+++ 화면설계/최효빈_화면설계/CalendarGrid.java
@@ -8,58 +8,92 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CalendarGrid extends Panel {
-	
-	CalendarGrid(){
+
+	CalendarGrid() {
 		ArrayList<Integer> calArr = new ArrayList<>();
+		ArrayList<Integer> prevArr = new ArrayList<>();
+		ArrayList<Integer> nextArr = new ArrayList<>();
+				
+		
 		Calendar cal = Calendar.getInstance();
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH);
 		int date = cal.get(Calendar.DATE);
 		int lastDay = cal.getActualMaximum(Calendar.DATE);
-		
-		/** 이번 달 세팅 **/
+
+		/** 이번 달 날짜를 배열에 추가 **/
 		for (int i = 0; i < lastDay; i++) {
 			calArr.add(i + 1);
 		}
 		
-		/** 지난 달 세팅 완료 **/
+		/** 지난 달 날짜를 배열에 추가 **/
 		cal.set(Calendar.DATE, 1);
 		int weekday = cal.get(Calendar.DAY_OF_WEEK);
 		int numOfPreMonth = weekday - 1;
 		cal.add(Calendar.MONTH, -1);
 		lastDay = cal.getActualMaximum(Calendar.DATE);
-		for(int i = 0; i < numOfPreMonth; i++) {
+		for (int i = 0; i < numOfPreMonth; i++) {
 			calArr.add(0, lastDay);
+			prevArr.add(0);
 			lastDay--;
 		}
-		
-		/**다음 달 세팅**/
+
+		/** 다음 달 날짜를 배열에 추가 **/
 		cal.add(Calendar.MONTH, 1);
 		lastDay = cal.getActualMaximum(Calendar.DATE);
 		cal.set(Calendar.DATE, lastDay);
 		weekday = cal.get(Calendar.DAY_OF_WEEK);
-		
+
 		int numOfNextMonth = 7 - weekday;
 		for (int i = 0; i < numOfNextMonth; i++) {
 			calArr.add(i + 1);
+			nextArr.add(0);
 		}
-		
-		/**프레임으로 구현**/
+
+		/** 프레임으로 구현 **/
 		GridLayout gridLayout = new GridLayout(calArr.size() / 7, 7);
 		setLayout(gridLayout);
-		
-		for(int i = 0; i < calArr.size(); i++) {
+
+		for (int i = 0; i < calArr.size(); i++) {
 			Button tempBtn = new Button(calArr.get(i) + "");
 			if (i % 7 == 0) {
 				tempBtn.setForeground(Color.RED);
-			} else if(i % 7 == 6) {
+			} else if (i % 7 == 6) {
 				tempBtn.setForeground(Color.BLUE);
 			}
+			
+			if (i < prevArr.size() || i >= calArr.size() - nextArr.size() )
+				tempBtn.setEnabled(false);
+			
 			add(tempBtn);
 		}
-		
+
 	}
-	
-	
-	
+
 }
+
+/*
+	*//** 프레임으로 구현 **//*
+						 * for(int i = 0; i < prevArr.size(); i++) { Button tempBtn = new
+						 * Button(prevArr.get(i) + ""); tempCal.setTime(null); if (i % 7 == 1) {
+						 * tempBtn.setForeground(Color.RED); } else if(i % 7 == 7) {
+						 * tempBtn.setForeground(Color.BLUE); }
+						 * 
+						 * add(tempBtn); }
+						 * 
+						 * for(int i = 0; i < calArr.size(); i++) { Button tempBtn = new
+						 * Button(calArr.get(i) + ""); if (i % 7 == 0) {
+						 * tempBtn.setForeground(Color.RED); } else if(i % 7 == 6) {
+						 * tempBtn.setForeground(Color.BLUE); }
+						 * 
+						 * add(tempBtn); }
+						 * 
+						 * for(int i = 0; i < nextArr.size(); i++) { Button tempBtn = new
+						 * Button(nextArr.get(i) + ""); if (i % 7 == 0) {
+						 * tempBtn.setForeground(Color.RED); } else if(i % 7 == 6) {
+						 * tempBtn.setForeground(Color.BLUE); }
+						 * 
+						 * add(tempBtn); } }
+						 * 
+						 * }
+						 */
