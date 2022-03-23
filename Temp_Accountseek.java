e364408be0dc7a5b477077030e364f372567acd9
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2022-03-23 16:33:19 +0900

    Create Temp_Accountseek.java

---
+++ Temp_Accountseek.java
@@ -0,0 +1,102 @@
+package ateamproject;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.SystemColor;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Accountseek {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Accountseek window = new Accountseek();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Accountseek() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 570, 470);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.textHighlight);
+		panel.setBounds(0, 0, 556, 433);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(177, 110, 196, 34);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514 \uCC3E\uAE30");
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		lblNewLabel.setForeground(SystemColor.text);
+		lblNewLabel.setBounds(36, 55, 66, 34);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC774\uBA54\uC77C");
+		lblNewLabel_2.setForeground(SystemColor.text);
+		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(36, 109, 66, 34);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("\uC774\uBA54\uC77C");
+		lblNewLabel_2_1.setForeground(SystemColor.text);
+		lblNewLabel_2_1.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		lblNewLabel_2_1.setBounds(36, 284, 66, 34);
+		panel.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638 \uCC3E\uAE30");
+		lblNewLabel_1.setForeground(SystemColor.text);
+		lblNewLabel_1.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(36, 230, 94, 34);
+		panel.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(177, 291, 196, 34);
+		panel.add(textField_1);
+		
+		JButton btnNewButton = new JButton("\uD655\uC778");
+		btnNewButton.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		btnNewButton.setBounds(402, 110, 66, 34);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\uD655\uC778");
+		btnNewButton_1.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(402, 292, 66, 34);
+		panel.add(btnNewButton_1);
+	}
+}
