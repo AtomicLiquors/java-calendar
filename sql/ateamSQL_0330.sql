44c9f44d7907aa36bcbb4efbc54ffe22d3218822
devdev2022 <100466989+devdev2022@users.noreply.github.com>
2022-03-30 10:50:48 +0900

    Update ateamSQL_0330.sql

--- sql/ateamSQL_0330.sql
+++ sql/ateamSQL_0330.sql
@@ -12,15 +12,25 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- 테이블 mydb.sched 구조 내보내기
+CREATE TABLE IF NOT EXISTS `sched` (
+  `sc_id` int NOT NULL AUTO_INCREMENT,
+  `sc_startdate` date NOT NULL,
+  `sc_title` char(30) NOT NULL,
+  `sc_enddate` date DEFAULT NULL,
+  `sc_content` char(200) DEFAULT NULL,
+  `sc_isdone` tinyint(1) DEFAULT NULL,
+  `sc_privacy` char(10) DEFAULT NULL,
+  `sc_priority` int DEFAULT NULL,
+  `sc_color` char(10) DEFAULT NULL,
+  PRIMARY KEY (`sc_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
 -- 테이블 데이터 mydb.sched:~2 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `sched` DISABLE KEYS */;
 INSERT INTO `sched` (`sc_id`, `sc_startdate`, `sc_title`, `sc_enddate`, `sc_content`, `sc_isdone`, `sc_privacy`, `sc_priority`, `sc_color`) VALUES
 	(1, '2022-03-23', '일정 제목', '2022-03-24', '일정 내용', NULL, NULL, NULL, NULL),
-	(2, '2022-03-24', '제목', '2022-03-28', '일정 내용2', NULL, NULL, NULL, NULL),
-	(3, '2022-03-05', '3', '2022-03-05', '내용', 0, NULL, 0, NULL),
-	(4, '2022-03-04', '새 일정 제목', '2022-03-04', '새 일정 내용', 0, NULL, 0, NULL),
-	(5, '2022-03-03', '새 일정 제목', '2022-03-03', 'asdf', 0, NULL, 0, NULL),
-	(6, '2022-03-02', '새 일정 제목', '2022-03-02', 'fdf', 0, NULL, 0, NULL);
+	(2, '2022-03-24', '제목', '2022-03-28', '일정 내용2', NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `sched` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
